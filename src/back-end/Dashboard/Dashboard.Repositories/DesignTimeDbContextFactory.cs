3beeb7272c5f71f62a28376410d493cb1428dcf9
Siarhei Kuchuk <cuchuk.sergey@gmail.com>
2019-10-29 14:14:24 +0300

    cleanup

--- src/back-end/Dashboard/Dashboard.Repositories/DesignTimeDbContextFactory.cs
+++ src/back-end/Dashboard/Dashboard.Repositories/DesignTimeDbContextFactory.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
-using System.Diagnostics;
 using System.IO;
 
 namespace Dashboard.Repositories
