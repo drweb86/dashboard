d0e4bbf7e8e61f52cdaa4099d2bb84f5edf95696
Siarhei Kuchuk <cuchuk.sergey@gmail.com>
2019-11-04 13:55:57 +0300

    create stub components

--- src/front-end/ClientApp/e2e/src/app.e2e-spec.ts
+++ src/front-end/ClientApp/e2e/src/app.e2e-spec.ts
@@ -6,9 +6,4 @@ describe('App', () => {
   beforeEach(() => {
     page = new AppPage();
   });
-
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getMainHeading()).toEqual('Hello, world!');
-  });
 });
