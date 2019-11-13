1f399a6eb0b895695ef88d315f3e9a542e41d163
Siarhei Kuchuk <cuchuk.sergey@gmail.com>
2019-11-13 20:21:24 +0300

    in progress

--- src/front-end/ClientApp/src/app/components/sticker/sticker.component.ts
+++ src/front-end/ClientApp/src/app/components/sticker/sticker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { StickerResultModel } from '../../models/stickers/sticker-result-model';
 
 @Component({
