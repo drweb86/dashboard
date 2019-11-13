597b69071fa1fc9eb953ac9ce61b395c9bf2d3f6
Siarhei Kuchuk <cuchuk.sergey@gmail.com>
2019-11-13 19:49:59 +0300

    about moved to right and became icon

--- src/front-end/ClientApp/src/app/components/app/app.component.ts
+++ src/front-end/ClientApp/src/app/components/app/app.component.ts
@@ -33,8 +33,8 @@ export class AppComponent implements OnInit, OnDestroy {
       title: 'About',
       url: '/about',
       visible: true,
-      right: false,
-      icon: undefined,
+      right: true,
+      icon: 'info',
     },
     {
       title: this._logoutName,
